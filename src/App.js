b4cf616617f58643913701200220dc1f7425e2f0
jussi_hyde <j.99hyytiainen@gmail.com>
2022-09-28 00:14:30 +0300

    tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [todo, setTodo] = useState({date: '', desc: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+    };
+  
+  
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	<header className="App-header">
+       <h1>Simple Todolist</h1>
       </header>
+      
+      <form onSubmit={addTodo}>
+        <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
+        <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+        <input type="submit" value="Add" />
+      </form>
+      <table>
+            <tbody>
+            <tr>
+                 <th>Date</th>
+                 <th>Description</th>
+            </tr>
+              {
+                todos.map((todo, index) =>
+                <tr key={index}>
+                  <td>{todo.date}</td>
+                  <td>{todo.desc}</td>
+                </tr>
+               )  
+              }         
+            </tbody>
+      </table>
     </div>
   );
 }
